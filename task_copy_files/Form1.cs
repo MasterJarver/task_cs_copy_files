287b065e8e8f37822d159e09b7e84e1ba461f9f3
Yuri Maxutenko <wolfcub@apriorit.com>
2019-05-24 11:36:13 +0300

    added create directory

--- task_copy_files/Form1.cs
+++ task_copy_files/Form1.cs
@@ -24,6 +24,8 @@ namespace task_copy_files
             if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 text_from.Text = dialog.SelectedPath;
+                from_path = dialog.SelectedPath;
+                from_dir_name = new System.IO.DirectoryInfo(dialog.SelectedPath).Name;
             }
         }
 
@@ -33,14 +35,18 @@ namespace task_copy_files
             if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 text_to.Text = dialog.SelectedPath;
+                to_path = dialog.SelectedPath;
+                to_dir_name = new System.IO.DirectoryInfo(dialog.SelectedPath).Name;
             }
         }
 
         async private void copy_Click(object sender, EventArgs e)
         {
             // string[] files = Directory.GetFiles(@"C:\1", "*.txt"); // получаем список всех файлов
-            string[] files = Directory.GetFiles(text_from.Text, "*.txt");
+            string[] files = Directory.GetFiles(from_path, "*");
+            Directory.CreateDirectory(to_path + @"\" + from_dir_name);
             int qt = files.Length;
+
             progressBar1.Maximum = qt; // устанавливаем максимальное значение прогресс бара по кол. файлов
             for(int i = 0; i < qt; i++)
             {
@@ -49,5 +55,9 @@ namespace task_copy_files
                 await Task.Delay(1000);
             }
         }
+        private string from_dir_name; // имя папки, которую нужно копировать
+        private string from_path; // путь к папке, которую нужно копировать
+        private string to_dir_name; // имя папки в которую нужно копировать
+        private string to_path; // путь к папке в которую нужно копировать
     }
 }
